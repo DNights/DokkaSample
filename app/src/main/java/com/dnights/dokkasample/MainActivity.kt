50768ba3ba6ce7d6d4fc8db4c951f01ff3a7da7e
hyun dong han <guehgk456@gmail.com>
2019-09-23 12:12:52 +0900

    add comment in MainActivity

--- app/src/main/java/com/dnights/dokkasample/MainActivity.kt
+++ app/src/main/java/com/dnights/dokkasample/MainActivity.kt
@@ -3,10 +3,28 @@ package com.dnights.dokkasample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * this is main activity
+ */
+
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * @param [savedInstanceState] default param
+     */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        test1()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    fun test1(){
+        println("test1")
     }
 }
